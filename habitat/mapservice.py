9b67054eb62d2aa84334e99c40f5b29205bc21e2
adorsk <adorsk>
2012-03-09 10:51:07 -0500

    basic working version

---
+++ habitat/mapservice.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, Response
+
+import sasi.sa.session as sa_session
+from sasi.dao.habitat.sa_habitat_dao import SA_Habitat_DAO
+import sasi.viz.habitat.map.mapserver as habitat_ms
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/getmap')
+def get_map():
+
+	# Get a session.
+	session = sa_session.get_session()
+
+	# Create habitat DAO.
+	habitat_dao = SA_Habitat_DAO(session=session)
+
+	# Parse parameters into custom and WMS parameters.
+	custom_parameters = request.args.get('PARAMS')
+	wms_parameters = request.args.items()
+
+	# Assemble filters from custom parameters.
+	# @TODO
+	filters = {}
+
+	# Generate map image for the given parameters.
+	map_image = habitat_ms.get_map_image_from_wms(wms_parameters=wms_parameters, habitat_dao=habitat_dao, filters=filters) 
+
+	# Return the image.
+	return Response(map_image, mimetype='image/gif')
+
+if __name__ == '__main__':
+	app.run()
+
