f72fab90f76643e3da9bfbb688a8f789963b1b3b
adorsk <adorsk>
2012-03-30 11:34:31 -0400

    added baselayers wsgi app launcher script, added caching

--- baselayers/baselayers_app.py
+++ baselayers/baselayers_app.py
@@ -1,15 +1,23 @@
 from flask import Flask, request, Response, json, jsonify
+from flaskext.cache import Cache
 import baselayers_services
 
 app = Flask(__name__)
 app.debug = True
 
+app.config['CACHE_TYPE'] = 'filesystem'
+app.config['CACHE_DIR'] = '/var/cache/sasi/basemap/app'
+cache = Cache(app)
+
 from xdomain import *
+
+def make_cache_key():
+	return "%s" % (request.url)
 
 @app.route('/get_map')
 @crossdomain(origin='*')
+@cache.cached(key_prefix=make_cache_key)
 def get_map():
-
 	wms_parameters = request.args.items()
 
 	map_image = baselayers_services.get_map(wms_parameters=wms_parameters)
